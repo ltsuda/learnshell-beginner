a0118165c6f5d079d1444d1e93dc38411188ce1e
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2020-03-23 16:57:00 -0300

    add 003 - passing arguments to script

---
+++ 003_passing_arguments_to_script/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# arguments to call this script
+# apple 5 banana 8 "Fruit Basket" 15
+
+echo $3
+
+BIG=$5
+
+echo "A $BIG costs just $6"
+
+echo "The number of arguments of this script are $#"
+
+echo "The arguments sent to me are: $@"
