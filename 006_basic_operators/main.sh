796af0a6bed4b6693e11f716e442a7f7cb2b6183
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2020-03-23 16:58:01 -0300

    add 006 - basic operators

---
+++ 006_basic_operators/main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+A=3
+B=$((100 * $A + 5))
+echo $B
+
+# a + b addition (a plus b)
+
+# a - b substraction (a minus b)
+
+# a * b multiplication (a times b)
+
+# a / b division (integer) (a divided by b)
+
+# a % b modulo (the integer remainder of a divided by b)
+
+# a ** b exponentiation (a to the power of b)
