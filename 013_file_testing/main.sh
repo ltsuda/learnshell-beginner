d92c4905e3a4b1ed9616c93b7a5a4e494cc063d6
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2020-03-24 12:45:38 -0300

    add 013 - file testing

---
+++ 013_file_testing/main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+filename="../README.md"
+if [ -e "$filename" ]; then
+    echo "$filename exists as a file"
+fi
+
+directory_name="../013_file_testing"
+if [ -d "$directory_name" ]; then
+    echo "$directory_name exists as a directory"
+fi
+
+filename="sample.md"
+if [ ! -f "$filename" ]; then
+    touch "$filename"
+fi
+if [ -r "$filename" ]; then
+    echo "you are allowed to read $filename"
+else
+    echo "you are not allowed to read $filename"
+fi
